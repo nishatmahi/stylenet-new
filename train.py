eeda21955ce9de5fc7ac5c676379cfc395855529
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2025-07-02 21:40:22 +0600

    Update train.py

--- train.py
+++ train.py
@@ -161,7 +161,7 @@ if __name__ == '__main__':
                         help='learning rate for caption model training')
     parser.add_argument('--lr_language', type=float, default=0.0005,
                         help='learning rate for language model training')
-    parser.add_argument('--epoch_num', type=int, default=20)
+    parser.add_argument('--epoch_num', type=int, default=15)
     parser.add_argument('--log_step_caption', type=int, default=50,
                         help='steps for print log while train caption model')
     parser.add_argument('--log_step_language', type=int, default=10,
