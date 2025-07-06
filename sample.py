2e0573e5d7c189123e987a0c74da4f1c516a5b2c
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2025-07-07 03:54:24 +0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -29,8 +29,10 @@ def main():
     decoder = FactoredLSTM(emb_dim, hidden_dim, factored_dim, tokenizer.vocab_size).to(device)
 
     # Load weights
-    encoder.load_state_dict(torch.load('/kaggle/working/pretrained_models/encoder-last.pkl', map_location=device))
-    decoder.load_state_dict(torch.load('/kaggle/working/pretrained_models/decoder-last.pkl', map_location=device))
+    model_dir = "stylenet_models"
+    encoder.load_state_dict(torch.load(f'{model_dir}/encoder-last.pkl', map_location=device))
+    decoder.load_state_dict(torch.load(f'{model_dir}/decoder-last.pkl', map_location=device))
+
     encoder.eval()
     decoder.eval()
 
