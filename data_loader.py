58e54c9659d4a563dc600f0f57c21da68bbe9b90
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2025-07-12 10:20:57 +0600

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -7,7 +7,7 @@ from torchvision import transforms
 from transformers import AutoTokenizer
 
 # ---- HuggingFace Bangla Tokenizer ----
-tokenizer = AutoTokenizer.from_pretrained("hishab/titulm-mpt-1b-v2.0", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("/kaggle/working/stylenet/tokenizer-extended", trust_remote_code=True)
 
 class Rescale:
     '''Rescale the image to a given size'''
