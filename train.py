d6c112126b95ed1243c6e9805c74df5b222fa230
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2025-07-21 15:17:37 +0600

    Update train.py

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ def eval_outputs(outputs, tokenizer):
 def main(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    permanent_save_folder = "stylenet_new_again_models/"
+    permanent_save_folder = "stylenet_new_models/"
     os.makedirs(permanent_save_folder, exist_ok=True)
     os.makedirs(args.model_path, exist_ok=True)
 
@@ -191,7 +191,7 @@ if __name__ == '__main__':
                         help='learning rate for caption model training')
     parser.add_argument('--lr_language', type=float, default=0.00004,
                         help='learning rate for language model training')
-    parser.add_argument('--epoch_num', type=int, default=40)
+    parser.add_argument('--epoch_num', type=int, default=10)
     parser.add_argument('--log_step_caption', type=int, default=100,
                         help='steps for print log while train caption model')
     parser.add_argument('--log_step_language', type=int, default=10,
