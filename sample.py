e8a75982f178ac19b29a15cf25116fa05b2ac8b7
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2025-07-29 21:57:40 +0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -89,7 +89,7 @@ def main():
     transform = transforms.Compose([
         Rescale((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
     ])
     img_dir = '/kaggle/input/sample-data/sample/sample_images'  # Change as needed
     reference_file = '/kaggle/input/sample-data/sample/sample_images_factual.txt'  # Add this path
