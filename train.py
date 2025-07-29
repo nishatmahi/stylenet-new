16adc2e61ede83e936507b3b43c63b6603f5213c
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2025-07-29 21:51:13 +0600

    Update train.py

--- train.py
+++ train.py
@@ -113,34 +113,34 @@ def main(args):
         eval_outputs(outputs, tokenizer)
 
         # styled (humorous)
-        if styled_data_loader:
-            for i, (captions, lengths) in enumerate(styled_data_loader):
-                captions = captions.long().to(device)
-                lengths = lengths.to(device)
-                decoder.zero_grad()
-                outputs = decoder(captions, features=None, mode='humorous')
-                loss = criterion(outputs, captions[:, 1:].contiguous(), lengths - 1)
-                loss.backward()
-                torch.nn.utils.clip_grad_norm_(lang_params, 1.0)
-                optimizer_lang.step()
-                if i % args.log_step_language == 0 or i == total_lang_step-1:
-                    print("Epoch [%d/%d], LANG, Step [%d/%d], Loss: %.4f"
-                          % (epoch+1, epoch_num, i, total_lang_step, loss.item()))
+        # if styled_data_loader:
+        #     for i, (captions, lengths) in enumerate(styled_data_loader):
+        #         captions = captions.long().to(device)
+        #         lengths = lengths.to(device)
+        #         decoder.zero_grad()
+        #         outputs = decoder(captions, features=None, mode='humorous')
+        #         loss = criterion(outputs, captions[:, 1:].contiguous(), lengths - 1)
+        #         loss.backward()
+        #         torch.nn.utils.clip_grad_norm_(lang_params, 1.0)
+        #         optimizer_lang.step()
+        #         if i % args.log_step_language == 0 or i == total_lang_step-1:
+        #             print("Epoch [%d/%d], LANG, Step [%d/%d], Loss: %.4f"
+        #                   % (epoch+1, epoch_num, i, total_lang_step, loss.item()))
 
-        # styled (romantic)
-        if styled_data_loader_romantic:
-            for i, (captions, lengths) in enumerate(styled_data_loader_romantic):
-                captions = captions.long().to(device)
-                lengths = lengths.to(device)
-                decoder.zero_grad()
-                outputs = decoder(captions, features=None, mode='romantic')
-                loss = criterion(outputs, captions[:, 1:].contiguous(), lengths - 1)
-                loss.backward()
-                torch.nn.utils.clip_grad_norm_(lang_params, 1.0)
-                optimizer_lang.step()
-                if i % args.log_step_language == 0 or i == total_romantic_step-1:
-                    print("Epoch [%d/%d], ROM, Step [%d/%d], Loss: %.4f"
-                          % (epoch+1, epoch_num, i, total_romantic_step, loss.item()))
+        # # styled (romantic)
+        # if styled_data_loader_romantic:
+        #     for i, (captions, lengths) in enumerate(styled_data_loader_romantic):
+        #         captions = captions.long().to(device)
+        #         lengths = lengths.to(device)
+        #         decoder.zero_grad()
+        #         outputs = decoder(captions, features=None, mode='romantic')
+        #         loss = criterion(outputs, captions[:, 1:].contiguous(), lengths - 1)
+        #         loss.backward()
+        #         torch.nn.utils.clip_grad_norm_(lang_params, 1.0)
+        #         optimizer_lang.step()
+        #         if i % args.log_step_language == 0 or i == total_romantic_step-1:
+        #             print("Epoch [%d/%d], ROM, Step [%d/%d], Loss: %.4f"
+        #                   % (epoch+1, epoch_num, i, total_romantic_step, loss.item()))
 
         # ======== SAVE: After every epoch =========
         os.makedirs(permanent_save_folder, exist_ok=True)
@@ -165,9 +165,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='StyleNet Bangla: Generating Attractive Visual Captions with Styles')
     parser.add_argument('--model_path', type=str, default='pretrained_models',
                         help='path for saving trained models')
-    parser.add_argument('--img_path', type=str, default='/kaggle/input/dataset/data/Flicker8k_Dataset',
+    parser.add_argument('--img_path', type=str, default='/kaggle/input/dataset/data/Images',
                     help='path for train images directory')
-    parser.add_argument('--factual_caption_path', type=str, default='/kaggle/input/dataset/data/factual_train.txt',
+    parser.add_argument('--factual_caption_path', type=str, default='/kaggle/input/dataset/data/factual_caption.txt',
                         help='path for factual caption file')
     parser.add_argument('--humorous_caption_path', type=str, default='/kaggle/input/dataset/data/humorous_train.txt',
                         help='path for humorous caption file')
