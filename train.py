98d370c6d801ea07bfeacd4a84835aa74ec446fc
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2025-08-04 23:22:32 +0600

    Update train.py

--- train.py
+++ train.py
@@ -185,15 +185,16 @@ if __name__ == '__main__':
                         help='size of factored matrix')
     parser.add_argument('--lr_caption', type=float, default=0.00002,
                         help='learning rate for caption model training')
-    parser.add_argument('--lr_language', type=float, default=0.00003,
+    parser.add_argument('--lr_language', type=float, default=0.00002,
                         help='learning rate for language model training')
-    parser.add_argument('--epoch_num', type=int, default=53)
+    parser.add_argument('--epoch_num', type=int, default=60)
     parser.add_argument('--log_step_caption', type=int, default=200,
                         help='steps for print log while train caption model')
     parser.add_argument('--log_step_language', type=int, default=10,
                         help='steps for print log while train language model')
     args = parser.parse_args()
     main(args)
+
 
 
 
