ab446955ce874279438be52b9c2e02f8d5fa49d5
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2025-08-11 01:33:21 +0600

    Update train.py

--- train.py
+++ train.py
@@ -83,7 +83,7 @@ def main(args):
     print("=============================")
 
     total_cap_step = len(data_loader)
-    total_lang_step = len(styled_data_loader) if styled_data_loader else 0
+    # total_lang_step = len(styled_data_loader) if styled_data_loader else 0
     total_romantic_step = len(styled_data_loader_romantic) if styled_data_loader_romantic else 0
     epoch_num = args.epoch_num
 
@@ -194,3 +194,4 @@ if __name__ == '__main__':
                         help='steps for print log while train language model')
     args = parser.parse_args()
     main(args)
+
