b60708d029acc1b721950a9a6df06c9947be9a0f
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2025-08-19 07:45:38 +0600

    Update train.py

--- train.py
+++ train.py
@@ -175,7 +175,7 @@ if __name__ == '__main__':
                         help='path for romantic caption file')
     parser.add_argument('--caption_batch_size', type=int, default=32,
                         help='mini batch size for caption model training')
-    parser.add_argument('--language_batch_size', type=int, default=64,
+    parser.add_argument('--language_batch_size', type=int, default=32,
                         help='mini batch size for language model training')
     parser.add_argument('--emb_dim', type=int, default=300,
                         help='embedding size of word, image')
@@ -185,7 +185,7 @@ if __name__ == '__main__':
                         help='size of factored matrix')
     parser.add_argument('--lr_caption', type=float, default=0.00002,
                         help='learning rate for caption model training')
-    parser.add_argument('--lr_language', type=float, default=0.00003,
+    parser.add_argument('--lr_language', type=float, default=0.00005,
                         help='learning rate for language model training')
     parser.add_argument('--epoch_num', type=int, default=30)
     parser.add_argument('--log_step_caption', type=int, default=200,
@@ -194,6 +194,7 @@ if __name__ == '__main__':
                         help='steps for print log while train language model')
     args = parser.parse_args()
     main(args)
+
 
 
 
