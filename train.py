b58412eb4731e5088330ae69df52eada1e5ef56d
nishatmahi <77322410+nishatmahi@users.noreply.github.com>
2025-08-21 22:35:27 +0600

    Update train.py

--- train.py
+++ train.py
@@ -165,15 +165,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='StyleNet Bangla: Generating Attractive Visual Captions with Styles')
     parser.add_argument('--model_path', type=str, default='pretrained_models',
                         help='path for saving trained models')
-    parser.add_argument('--img_path', type=str, default='/kaggle/input/dataset/data/Images',
+    parser.add_argument('--img_path', type=str, default='/kaggle/input/dataset/data/Flicker8k_Dataset',
                     help='path for train images directory')
-    parser.add_argument('--factual_caption_path', type=str, default='/kaggle/input/dataset/data/factual_caption.txt',
+    parser.add_argument('--factual_caption_path', type=str, default='/kaggle/input/dataset/data/factual_train.txt',
                         help='path for factual caption file')
     parser.add_argument('--humorous_caption_path', type=str, default='/kaggle/input/dataset/data/humorous_text.txt',
                         help='path for humorous caption file')
-    parser.add_argument('--romantic_caption_path', type=str, default='/kaggle/input/dataset/data/romantic_data.txt',
+    parser.add_argument('--romantic_caption_path', type=str, default='/kaggle/input/dataset/data/romantic_text.txt',
                         help='path for romantic caption file')
-    parser.add_argument('--caption_batch_size', type=int, default=64,
+    parser.add_argument('--caption_batch_size', type=int, default=32,
                         help='mini batch size for caption model training')
     parser.add_argument('--language_batch_size', type=int, default=32,
                         help='mini batch size for language model training')
@@ -185,7 +185,7 @@ if __name__ == '__main__':
                         help='size of factored matrix')
     parser.add_argument('--lr_caption', type=float, default=0.00002,
                         help='learning rate for caption model training')
-    parser.add_argument('--lr_language', type=float, default=0.00005,
+    parser.add_argument('--lr_language', type=float, default=0.00004,
                         help='learning rate for language model training')
     parser.add_argument('--epoch_num', type=int, default=30)
     parser.add_argument('--log_step_caption', type=int, default=200,
@@ -194,6 +194,7 @@ if __name__ == '__main__':
                         help='steps for print log while train language model')
     args = parser.parse_args()
     main(args)
+
 
 
 
